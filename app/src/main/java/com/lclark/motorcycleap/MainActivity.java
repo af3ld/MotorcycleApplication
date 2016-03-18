30b48894051aa17ca630f01f451be84a320880f1
af3ld <alexfeldman@lclark.edu>
2016-03-17 19:23:29 -0700

    Pulling

--- app/src/main/java/com/lclark/motorcycleap/MainActivity.java
+++ app/src/main/java/com/lclark/motorcycleap/MainActivity.java
@@ -5,19 +5,29 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String API_KEY = "AIzaSyC2WPTne8JjQTUEmW5ck9ymeiZFJ3LQjL0";
     public static final String API_KEY_TITLE = "Android_Maps_key_1";
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeMap();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -30,6 +40,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    /**
+     * Creates new version if map is not null
+     */
+    private void initializeMap() {
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.fragment_map)).getMap();
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(), "Unable to create maps", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
