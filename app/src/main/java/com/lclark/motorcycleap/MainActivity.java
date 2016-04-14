7f8fb80e1e5a4d79ef0ef0046a14f0c0ca87733a
af3ld <alexfeldman@lclark.edu>
2016-04-13 23:41:34 -0700

    Basic tabs in place

--- app/src/main/java/com/lclark/motorcycleap/MainActivity.java
+++ app/src/main/java/com/lclark/motorcycleap/MainActivity.java
@@ -1,7 +1,6 @@
 package com.lclark.motorcycleap;
 
 
-import android.app.ActionBar;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
@@ -9,19 +8,18 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 
-import android.support.v4.app.FragmentActivity;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
 
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback{
+
+public class MainActivity extends AppCompatActivity{
 
     public static final String API_KEY = "AIzaSyC2WPTne8JjQTUEmW5ck9ymeiZFJ3LQjL0";
     public static final String API_KEY_TITLE = "Android_Maps_key_1";
@@ -32,11 +30,9 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(this);
 
+        setUpViewPager();
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.activity_main_viewpager);
 
         // SENSOR LOGIC: RYAN
         SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -57,8 +53,16 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    public void setUpViewPager(){
+        ViewPager viewPager = (ViewPager) findViewById(R.id.activity_main_viewpager);
+        FragmentPagerAdapter adapter = new TabAdapter(getSupportFragmentManager(), this);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.activity_main_tablayout);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
     }
+
 }
